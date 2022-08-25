d53196d544f6fec71fd88587853112b4728cf832
Exception568 <hashcatx568@gmail.com>
2022-08-25 21:11:27 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+import requests
+import threading
+import time
+session = requests.Session()
+
+time.sleep(2)
+webhook = input("enter webhook url: ")
+message = input("enter the message to send: ")
+username = input("enter the webhook's username: ")
+def hook():
+    session.post(webhook,json = {"content":message,"username":username})
+    
+    while True:
+        for i in range(23):
+            threading.Thread(target=hook).start()
+hook()
+
+for i in range(23):
+    threading.Thread(target=hook).start()
+hook()
